e8ecaee8d185bcca295cde7bad301ed14a465e3c
Marcus Gaspar <viniciusgaspar01@gmail.com>
2022-06-06 16:58:15 -0300

    correção sessão banner princial

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+
+//animações scroll revela
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.about-developer', {duration:2050});
+sr.reveal('.skills', {duration:2050});
+sr.reveal('.content-skills', {delay: 500, rotate: { x: 100, y: 100, z: 0}, duration:2050});
+sr.reveal('.services', {duration:2050});
+sr.reveal('.formations', {duration:2050});
+sr.reveal('.contact', {duration:2050});
+
+
+//menu-mobile
